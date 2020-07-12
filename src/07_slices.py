87db58f729d0f04f469bee0669ab90e7c5ce6c7d
Jose <pjose14.jp38@yahoo.com>
2020-07-12 00:13:54 -0500

    all done with project

--- src/07_slices.py
+++ src/07_slices.py
@@ -2,10 +2,8 @@
 Python exposes a terse and intuitive syntax for performing 
 slicing on lists and strings. This makes it easy to reference
 only a portion of a list or string. 
-
 This Stack Overflow answer provides a brief but thorough
 overview: https://stackoverflow.com/a/509295
-
 Use Python's slice syntax to achieve the following:
 """
 
@@ -15,19 +13,21 @@ a = [2, 4, 1, 7, 9, 6]
 print(a[1])
 
 # Output the second-to-last element: 9
-print(a[-2])
+print(a[-2])  # OR print(a[len(a)-2:len(a)-1])
 
 # Output the last three elements in the array: [7, 9, 6]
 print(a[-3:])
 
 # Output the two middle elements in the array: [1, 7]
-print(a[2:4])
+first = int(len(a)/2 - 1)
+last = int(len(a)/2 + 1)
+print(a[first:last])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
 print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print(a[-1:])
+print(a[:-1])
 
 # For string s...
 
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,7 +1,6 @@
 """
 Python makes performing file I/O simple. Take a look
 at how to read and write to files here:
-
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
@@ -10,9 +9,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-foo = open('foo.txt', "r")
-print(foo.read())
-foo.close()
+import os.path
+
+#get the file path relative to the current script
+my_path = os.path.dirname(__file__)
+foo_path = os.path.join(my_path, 'foo.txt')
+
+with open(foo_path) as f:
+    print('Contents of \'foo.txt\':')
+    for line in f:
+        print('   ' +line, end = '')
+
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -20,10 +28,22 @@ foo.close()
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
-boo = open("bar.text", "w")
-boo.write("\n I'm line 1. \n I'm line 2. \n I'm line 3.")
-boo.close()
+arbitrary_text = '''First line of arbitrary text.
+Second line of arbitrary text.
+Third line of arbitrary text.'''
 
-boo= open("bar.text", "r")
-print(boo.read())
-boo.close
+bar_path = os.path.join(my_path, 'bar.txt')
+
+# Create (or overrite existing) file and write arbitrary text
+with open(bar_path, 'w') as f:
+    f.write(arbitrary_text)
+
+f.close()
+
+# Open newly created file and print its contents
+with open(bar_path) as f:
+    print('\nContents of \'bar.txt\':')
+    for line in f:
+        print('    ' + line, end='')
+
+f.close()
